03335c2ba7dc000963f887a9a994815ceb07b2a6
Matti-Krebelder <matti.kre@gmx.de>
2024-09-24 22:26:44 +0200

    updated

--- static/js/loader.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    const overlay = document.getElementById("overlay");
-    setTimeout(() => {
-        overlay.style.opacity = 0;
-    }, 3500);
-    setTimeout(() => {
-        overlay.style.display = "none";
-    }, 4500);
-};
